25a7a271b11c6c9c89731244b03f7eda0333f705
seanstark-ms <84108246+seanstark@users.noreply.github.com>
2022-11-09 13:56:57 -0800

    Update update-winevent-dcr-to-secevents.ps1

--- data_collection_rules/update-winevent-dcr-to-secevents.ps1
+++ data_collection_rules/update-winevent-dcr-to-secevents.ps1
@@ -1,16 +1,20 @@
+<#
+ .EXAMPLE
+ .\update-winevent-dcr-to-secevents.ps1 -subscriptionId 'ada06e68-375e-4210-be3a-c6cacebf41c5' -resourceGroup 'sentinel-dcrs' -ruleName 'windows-events'
+#>
+
 param(
-    [Parameter(Mandatory=$true]
-    [string]$subscriptionId 
+    [Parameter(Mandatory=$true)]
+    [string]$subscriptionId,
 
-    [Parameter(Mandatory=$true]
-    [string]$resourceGroup
+    [Parameter(Mandatory=$true)]
+    [string]$resourceGroup,
 
-    [Parameter(Mandatory=$true]
-    [string]$ruleName 
+    [Parameter(Mandatory=$true)]
+    [string]$ruleName,
 
-    [Parameter(Mandatory=$false]
+    [Parameter(Mandatory=$false)]
     [string]$apiVersion = '2021-09-01-preview'
-
 )
 
 $uri = ('https://management.azure.com/subscriptions/{0}/resourceGroups/{1}/providers/Microsoft.Insights/dataCollectionRules/{2}?api-version={3}' -f $subscriptionId, $resourceGroup, $ruleName, $apiVersion)
@@ -22,4 +26,5 @@ $dcr = (Invoke-AzRestMethod -Uri $uri).content | ConvertFrom-Json -Depth 20
 ($dcr.properties.dataFlows | Where streams -like 'Microsoft-Event').streams = @('Microsoft-SecurityEvent')
 
 # Update the DCR
-Invoke-AzRestMethod -Uri $uri -Method PUT -Payload ($payload | ConvertTo-Json -Depth 20)
+Invoke-AzRestMethod -Uri $uri -Method PUT -Payload ($dcr | ConvertTo-Json -Depth 20)
+
