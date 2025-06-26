295973d7b17d1853f6a6dca1f1e203ac66f93b1f
seanstark-ms <84108246+seanstark@users.noreply.github.com>
2025-06-26 12:09:10 -0700

    Update copy-logAnalyticsTable.ps1
    Added the ability to copy a table from one workspace to another

--- table_tools/copy-logAnalyticsTable.ps1
+++ table_tools/copy-logAnalyticsTable.ps1
@@ -8,21 +8,34 @@
  .PARAMETER newTableName
     Specify the name of the new table
 
- .PARAMETER subscriptionID
+ .PARAMETER sourceSubscriptionID
     Specify the subscriptionID of your log analytics workspace 
 
- .PARAMETER resourceGroupName
+ .PARAMETER sourceResourceGroupName
     Specify the Resource Group Name of your log analytics workspace
 
- .PARAMETER workspaceName
+ .PARAMETER sourceWorkspaceName
     Specify the log analytics workspace name where the source table resides
+
+  .PARAMETER destSubscriptionID
+    Specify the subscriptionID of the destination log analytics workspace 
+
+ .PARAMETER destResourceGroupName
+    Specify the Resource Group Name of the destination log analytics workspace
+
+ .PARAMETER destWorkspaceName
+    Specify the log analytics workspace name where the destination source table resides
     
  .PARAMETER apiVersion
     Specify the apiVersion to use, not required
 
  .EXAMPLE
-    Create a new log analytics table from an existing one
+    Create a new log analytics table from an existing one in the same workspace
     .\copy-logAnalyticsTable.ps1 -sourceTableName 'Okta_CL' -newTableName 'Okta_AppTeam_CL' -subscriptionID 'ada06dd8-375e-4210-be3a-c6cdde3341c5' -resourceGroupName 'sentinel' -workspaceName 'sentinel-prd'
+
+    Create a new log analytics table from an existing one in the same workspace
+    ./copy-logAnalyticsTable.ps1 -sourceTableName "CommonSecurityLog" -newTableName "CustomCEF_CL" -sourceSubscriptionID "9ebcb583-7adb-888a-b688-06f1de624cc1" -sourceResourceGroupName "sentinel-prd" -sourceWorkspaceName "sentinel-prd" -destSubscriptionID "9ebcb583-7adb-888a-b688-06f1de624cc1" -destResourceGroupName "app-team-rg" -destWorkspaceName "app-team"
+
 #>
 
 param(
@@ -33,21 +46,30 @@ param(
     [string]$newTableName,
 
     [Parameter(Mandatory=$true)]
-    [string]$subscriptionID,
+    [string]$sourceSubscriptionID,
 
     [Parameter(Mandatory=$true)]
-    [string]$resourceGroupName,
+    [string]$sourceResourceGroupName,
 
     [Parameter(Mandatory=$true)]
-    [string]$workspaceName,
+    [string]$sourceWorkspaceName,
+
+    [Parameter(ParameterSetName='DifferentLAW',Mandatory=$false)]
+    [string]$destSubscriptionID,
+
+    [Parameter(ParameterSetName='DifferentLAW',Mandatory=$false)]
+    [string]$destResourceGroupName,
+
+    [Parameter(ParameterSetName='DifferentLAW',Mandatory=$false)]
+    [string]$destWorkspaceName,
 
     [Parameter(Mandatory=$false)]
-    [string]$apiVersion = '2021-12-01-preview'
+    [string]$apiVersion = '2023-09-01'
 
 )
 
 #Get source table
-$sourceTable = Invoke-AzRestMethod -Path ('/subscriptions/{0}/resourcegroups/{1}/providers/microsoft.operationalinsights/workspaces/{2}/tables/{3}?api-version=2021-12-01-preview' -f $subscriptionID, $resourceGroupName, $workspaceName, $sourceTableName, $apiVersion) -Method GET
+$sourceTable = Invoke-AzRestMethod -Path ('/subscriptions/{0}/resourcegroups/{1}/providers/microsoft.operationalinsights/workspaces/{2}/tables/{3}?api-version=2021-12-01-preview' -f $sourceSubscriptionID, $sourceResourceGroupName, $sourceWorkspaceName, $sourceTableName, $apiVersion) -Method GET
 
 # Convert From Json
 $sourceTable = $sourceTable.Content | ConvertFrom-Json
@@ -58,11 +80,27 @@ If (!($newTableName.EndsWith('_CL'))){
 }
 
 # Udpate Schema with new table name and look for default workspace retention properties
-$sourceTable.properties.schema.Name = $newTableName
+$customColumns = $sourceTable.properties.schema.standardColumns | Where name -notin ('TenantId')
+Add-Member -InputObject $sourceTable.properties.schema -Type NoteProperty -Name 'columns' -Value @($customColumns)
+$sourceTable.name = $newTableName
+$sourceTable.properties.schema.name = $newTableName
+$sourceTable.properties.schema.displayName = $newTableName
+$sourceTable.properties.schema.tableType = 'CustomLog'
+$sourceTable.properties.schema.tableSubType = 'DataCollectionRuleBased'
 If ($sourceTable.properties.retentionInDaysAsDefault){
     $sourceTable.properties.totalRetentionInDays = $null
     $sourceTable.properties.retentionInDays = -1
 }
+#$sourceTable.properties.schema.standardColumns = $standardColumns
+$sourceTable.properties.schema.PSObject.Properties.Remove('standardColumns')
+$sourceTable.properties.schema.PSObject.Properties.Remove('solutions')
+$sourceTable.PSObject.Properties.Remove('id')
 
 #Create the new table
-Invoke-AzRestMethod -Path ('/subscriptions/{0}/resourcegroups/{1}/providers/microsoft.operationalinsights/workspaces/{2}/tables/{3}?api-version=2021-12-01-preview' -f $subscriptionID, $resourceGroupName, $workspaceName, $newTableName, $apiVersion) -Method PUT -Payload $($sourceTable | ConvertTo-Json -Depth 6)
+If ($destSubscriptionID) {
+   Write-Host "Creating new table $newTableName in workspace $destWorkspaceName in resource group $destResourceGroupName in subscription $destSubscriptionID"
+   Invoke-AzRestMethod -Path ('/subscriptions/{0}/resourcegroups/{1}/providers/microsoft.operationalinsights/workspaces/{2}/tables/{3}?api-version=2021-12-01-preview' -f $destSubscriptionID, $destResourceGroupName, $destWorkspaceName, $newTableName, $apiVersion) -Method PUT -Payload $($sourceTable | ConvertTo-Json -Depth 6)
+}else {
+   Write-Host "Creating new table $newTableName in workspace $sourceWorkspaceName in resource group $sourceResourceGroupName in subscription $sourceSubscriptionID"
+   Invoke-AzRestMethod -Path ('/subscriptions/{0}/resourcegroups/{1}/providers/microsoft.operationalinsights/workspaces/{2}/tables/{3}?api-version=2021-12-01-preview' -f $sourceSubscriptionID, $sourceResourceGroupName, $sourceWorkspaceName, $newTableName, $apiVersion) -Method PUT -Payload $($sourceTable | ConvertTo-Json -Depth 6)
+}
